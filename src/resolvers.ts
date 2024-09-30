f19125d58af08974c07b6ce114fa06771f0a9c30
Sam Dawson <samueljohndawson@gmail.com>
2024-09-30 19:21:18 +0100

    update resolvers to use csv data instead of stub data

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,29 +1,8 @@
-import { Product, Customer } from "./AppStateTypes";
-
-// TODO - Remove this stub data
-const products: Product[] = [
-  {
-    vin: "1C6RR6LT9DS578427",
-    colour: "White",
-    make: "Landrover",
-    model: "Evoque",
-    price: "52000",
-  },
-];
-
-const customers: Customer[] = [
-  {
-    email: "tom.harding1974@gmail.co.uk",
-    forename: "Tom",
-    surname: "Harding",
-    contact_number: "07938244758",
-    postcode: "SS26GH",
-  },
-];
+import csvToObjects from "./helperFunctions";
 
 export const resolvers = {
   Query: {
-    products: () => products,
-    customers: () => customers,
+    products: () => csvToObjects("product.csv"),
+    customers: () => csvToObjects("customer.csv"),
   },
 };
