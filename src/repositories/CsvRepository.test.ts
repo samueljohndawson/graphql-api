3c79395e24ba9ae3a293971c4f083679431060cb
Sam Dawson <samueljohndawson@gmail.com>
2024-10-01 18:36:32 +0100

    refactor csvRepository tests to correctly mock read/write helper functions

--- src/repositories/CsvRepository.test.ts
+++ src/repositories/CsvRepository.test.ts
@@ -1,54 +1,21 @@
 import { describe, expect, it, jest, beforeEach } from "@jest/globals";
-import * as fs from "fs";
 import { CsvRepository } from "./CsvRepository";
+import * as helperFunctions from "../helperFunctions";
 
-jest.mock("fs");
+jest.mock("../helperFunctions");
 describe("CsvRepository", () => {
+  let newData: any = {};
   beforeEach(() => {
-    const mockData = "key1,key2,key3\nvalue1,value2,value3";
-    (fs.readFileSync as jest.Mock).mockReturnValue(mockData);
+    jest.clearAllMocks();
+    newData = {
+      key1: "new_value1",
+      key2: "new_value2",
+      key3: "new_value3",
+    };
+    (helperFunctions.csvToObjects as jest.Mock).mockReturnValue(newData);
+    (helperFunctions.writeObjectToCsv as jest.Mock).mockReturnValue(newData);
   });
 
-  it("reads from product csv when type is product", () => {
-    // Given
-    const repo = new CsvRepository("product");
-
-    // When
-    const result = repo.read();
-    const expected = [
-      {
-        key1: "value1",
-        key2: "value2",
-        key3: "value3",
-      },
-    ];
-
-    // Then
-    expect(fs.readFileSync).toHaveBeenCalledWith("./data/csv/product.csv", {
-      encoding: "utf8",
-    });
-    expect(result).toStrictEqual(expected);
-  });
-  it("reads from product csv when type is customer", () => {
-    // Given
-    const repo = new CsvRepository("customer");
-
-    // When
-    const result = repo.read();
-
-    // Then
-    expect(fs.readFileSync).toHaveBeenCalledWith("./data/csv/customer.csv", {
-      encoding: "utf8",
-    });
-    const expected = [
-      {
-        key1: "value1",
-        key2: "value2",
-        key3: "value3",
-      },
-    ];
-    expect(result).toStrictEqual(expected);
-  });
   it("should log an error message when type is not recognized", () => {
     // Given
     const consoleErrorSpy = jest.spyOn(console, "error");
@@ -62,41 +29,51 @@ describe("CsvRepository", () => {
       "type not recognized: unknown"
     );
   });
+
+  it("reads from product csv when type is product", () => {
+    // Given
+    const repo = new CsvRepository("product");
+
+    // When
+    repo.read();
+
+    // Then
+    expect(helperFunctions.csvToObjects).toHaveBeenCalledWith("product.csv");
+  });
+  it("reads from customer csv when type is customer", () => {
+    // Given
+    const repo = new CsvRepository("customer");
+
+    // When
+    repo.read();
+
+    // Then
+    expect(helperFunctions.csvToObjects).toHaveBeenCalledWith("customer.csv");
+  });
   it("writes to product csv when type is product", () => {
     // Given
     const repo = new CsvRepository("product");
-    const newData = {
-      key1: "new_value1",
-      key2: "new_value2",
-      key3: "new_value3",
-    };
-
-    // Mock the write method
-    const writeMock = jest.spyOn(repo, "write");
 
     // When
     repo.write(newData);
 
     // Then
-    expect(writeMock).toHaveBeenCalled();
+    expect(helperFunctions.writeObjectToCsv).toHaveBeenCalledWith(
+      newData,
+      "product.csv"
+    );
   });
-
   it("writes to customer csv when type is customer", () => {
     // Given
     const repo = new CsvRepository("customer");
-    const newData = {
-      key1: "new_value1",
-      key2: "new_value2",
-      key3: "new_value3",
-    };
-
-    // Mock the write method
-    const writeMock = jest.spyOn(repo, "write");
 
     // When
     repo.write(newData);
 
     // Then
-    expect(writeMock).toHaveBeenCalled();
+    expect(helperFunctions.writeObjectToCsv).toHaveBeenCalledWith(
+      newData,
+      "customer.csv"
+    );
   });
 });
